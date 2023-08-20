0c4716cec658557c652df65b2cdb96876d4c7bf3
Katelyn Gadd <kg@luminance.org>
2023-08-20 02:10:30 -0700

    The basics all work now

---
+++ FStringCompiler/FStrings.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Squared.Util.Text;
+
+namespace Squared.FString {
+    public interface IFString {
+        string Name { get; }
+        void EmitValue (ref FStringBuilder output, string id);
+        void AppendTo (ref FStringBuilder output, FStringDefinition definition);
+        void AppendTo (StringBuilder output);
+    }
+
+    public class FStringTable {
+        public static FStringTable Default = new FStringTable("empty");
+
+        public readonly string Name;
+        private readonly Dictionary<string, FStringDefinition> Entries 
+            = new Dictionary<string, FStringDefinition>(StringComparer.OrdinalIgnoreCase);
+
+        public FStringTable (string name) {
+            Name = name;
+        }
+
+        public FStringTable (string name, Stream input) 
+            : this (name) {
+            var xrs = new XmlReaderSettings {
+                CloseInput = false,
+            };
+            using (var xr = XmlReader.Create(input, xrs)) {
+                // Seek to first file and then read each file
+                while (xr.ReadToFollowing("File"))
+                    PopulateFromXmlNode(xr);
+            }
+        }
+
+        private void PopulateFromXmlNode (XmlReader xr) {
+            while (xr.Read()) {
+                switch (xr.NodeType) {
+                    case XmlNodeType.Element:
+                        var name = xr.Name;
+                        var formatString = xr.ReadElementContentAsString();
+                        Add(name, formatString);
+                        break;
+                    case XmlNodeType.Whitespace:
+                    case XmlNodeType.Comment:
+                        break;
+                    case XmlNodeType.EndElement:
+                        if (xr.Name == "File")
+                            return;
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+
+        public FStringDefinition Add (string name, string formatString) {
+            var definition = FStringDefinition.Parse(name, formatString);
+            Entries.Add(name, definition);
+            return definition;
+        }
+
+        public FStringDefinition Get (string name, bool optional = true) {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+
+            if (!Entries.TryGetValue(name, out var entry))
+                return FStringDefinition.Missing(name);
+            else
+                return entry;
+        }
+    }
+
+    public class FStringDefinition {
+        public readonly string Name;
+        public readonly List<(bool emit, string textOrId)> Opcodes = 
+            new List<(bool emit, string textOrId)>();
+
+        protected FStringDefinition (string name) {
+            Name = name;
+        }
+
+        private static char GetChar (string s, int index) {
+            if ((index < 0) || (index >= s.Length))
+                return '\0';
+            else
+                return s[index];
+        }
+
+        public static FStringDefinition Parse (string name, string formatString) {
+            var result = new FStringDefinition(name);
+
+            var buildingEmit = false;
+            int segmentStart = 0;
+            for (int i = 0; i < formatString.Length; i++) {
+                switch (formatString[i]) {
+                    case '{':
+                        if (buildingEmit)
+                            throw new Exception($"Unexpected '{{' inside of expansion value in string {name}");
+
+                        AddSegment(i);
+
+                        if (GetChar(formatString, i + 1) == '{') {
+                            i++;
+                            result.Opcodes.Add((false, "{"));
+                        } else {
+                            buildingEmit = true;
+                        }
+                        segmentStart = i + 1;
+                        break;
+
+                    case '}':
+                        var wasBuildingEmit = buildingEmit;
+                        AddSegment(i);
+
+                        if (GetChar(formatString, i + 1) == '}') {
+                            if (wasBuildingEmit)
+                                throw new Exception($"Unexpected '}}' inside of expansion value in string {name}");
+                            i++;
+                            result.Opcodes.Add((false, "}"));
+                        }
+
+                        segmentStart = i + 1;
+                        break;
+                }
+            }
+
+            AddSegment(formatString.Length);
+
+            void AddSegment (int end) {
+                if (end <= segmentStart)
+                    return;
+
+                // Create an ImmutableAbstractString with a pre-populated hash code.
+                var text = formatString.Substring(segmentStart, end - segmentStart);
+                if (buildingEmit)
+                    text = string.Intern(text);
+                result.Opcodes.Add((buildingEmit, text));
+                buildingEmit = false;
+                segmentStart = end;
+            }
+
+            return result;
+        }
+
+        public static FStringDefinition Missing (string name) {
+            return new FStringDefinition(name) {
+                Opcodes = {
+                    (false, "MISSING: "),
+                    (false, name),
+                },
+            };
+        }
+
+        public void AppendTo<TInstance> (ref TInstance instance, ref FStringBuilder output)
+            where TInstance : IFString 
+        {
+            foreach (var opcode in Opcodes) {
+                if (opcode.emit)
+                    instance.EmitValue(ref output, opcode.textOrId);
+                else
+                    output.Append(opcode.textOrId);
+            }
+        }
+    }
+
+    public struct FStringBuilder {
+        private static readonly ThreadLocal<StringBuilder> ScratchBuilder = new ThreadLocal<StringBuilder>(() => new StringBuilder());
+
+        internal bool OwnsOutput;
+        internal StringBuilder Output;
+        internal string Result;
+
+        public FStringBuilder (StringBuilder output) {
+            OwnsOutput = false;
+            Output = output;
+            Result = null;
+        }
+
+        private StringBuilder O {
+            get {
+                if (Result != null)
+                    throw new InvalidOperationException("String already built");
+                else if (Output == null) {
+                    OwnsOutput = true;
+                    Output = ScratchBuilder.Value ?? new StringBuilder();
+                    ScratchBuilder.Value = null;
+                }
+
+                return Output;
+            }
+        }
+
+        public void Append (char ch) {
+            O.Append(ch);
+        }
+
+        public void Append (string text) {
+            O.Append(text);
+        }
+
+        public void Append (AbstractString text) {
+            text.CopyTo(O);
+        }
+
+        public void Append<T> (T value) {
+            var t = typeof(T);
+            if (t.IsEnum) {
+                // FIXME
+                O.Append(value);
+            } else if (t.IsValueType) {
+                // FIXME: Numeric types
+                O.Append(value);
+            } else {
+                O.Append(value);
+            }
+        }
+
+        public override string ToString () {
+            if (Result != null)
+                return Result;
+            else if (Output == null)
+                return null;
+
+            Result = Output.ToString();
+            if (OwnsOutput) {
+                Output.Clear();
+                ScratchBuilder.Value = Output;
+            }
+            Output = null;
+            return Result;
+        }
+
+        public static implicit operator FStringBuilder (StringBuilder stringBuilder)
+            => new FStringBuilder(stringBuilder);
+    }
+}
--- FStringCompiler/Program.cs
+++ FStringCompiler/Program.cs
@@ -1,92 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
+using Squared.FString;
 
 namespace FStringCompiler {
     class Program {
         private static readonly Regex UsingRegex = new Regex(@"using .+?;", RegexOptions.Compiled | RegexOptions.ExplicitCapture),
-            FunctionSignatureRegex = new Regex(@"\(((?<type>\w+)\s+(?<name>\w+)\s*,?\s*)*\)\s*=>\s*\{", RegexOptions.Compiled | RegexOptions.ExplicitCapture),
+            FunctionSignatureRegex = new Regex(@"\(((?<type>(\w|\?)+)\s+(?<name>\w+)\s*,?\s*)*\)\s*=>\s*\{", RegexOptions.Compiled | RegexOptions.ExplicitCapture),
             StringRegex = new Regex("(?<name>\\w+)\\s*=\\s*\"(?<text>(\\.|[^\"\\n])*)\";", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
 
         public static void Main (string[] args) {
-            if (args.Length != 2) {
-                Console.Error.WriteLine("Usage: fstringcompiler [input directory] [output directory]");
+            if (args.Length != 3) {
+                Console.Error.WriteLine("Usage: fstringcompiler [input directory] [output directory] [ISO language name]");
                 Environment.Exit(1);
             }
 
-            foreach (var inputFile in Directory.EnumerateFiles(args[0], "*.fstring", SearchOption.AllDirectories)) {
-                int ln = 0;
-                var inClass = false;
-                var outPath = Path.Combine(args[1], Path.GetFileNameWithoutExtension(inputFile) + ".cs");
-                StringGroup group = null;
-                using (var input = new StreamReader(inputFile))
-                using (var output = new StreamWriter(outPath, false, Encoding.UTF8)) {
-                    while (!input.EndOfStream) {
-                        var line = input.ReadLine();
-                        ln++;
-                        if (string.IsNullOrWhiteSpace(line)) {
-                            output.WriteLine();
-                            continue;
-                        }
+            var sourceDir = Path.GetFullPath(args[0]);
+            var started = DateTime.UtcNow;
+            var xws = new XmlWriterSettings {
+                Indent = true,
+                Encoding = Encoding.UTF8,
+                NewLineHandling = NewLineHandling.Entitize,
+                CloseOutput = true,
+                WriteEndDocumentOnClose = true,
+            };
+            using (var xmlWriter = XmlWriter.Create(Path.Combine(args[1], $"FStringTable_{args[2]}.xml"), xws)) {
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("FStringTable");
+                xmlWriter.WriteAttributeString("GeneratedUtc", started.ToString("o"));
 
-                        line = line.Trim();
-                        if (line.StartsWith("//")) {
-                            output.WriteLine(line);
-                            continue;
-                        }
+                foreach (var inputFile in Directory.EnumerateFiles(sourceDir, "*.fstring", SearchOption.AllDirectories)) {
+                    xmlWriter.WriteStartElement("File");
 
-                        try {
-                            if (group == null) {
-                                if (UsingRegex.IsMatch(line)) {
-                                    if (inClass) {
-                                        Console.Error.WriteLine($"{args[0]}({ln}): Cannot add new using statements after defining a string");
-                                        Environment.Exit(5);
+                    var shortPath = Path.GetFullPath(inputFile).Replace(sourceDir, "");
+                    xmlWriter.WriteAttributeString("SourcePath", shortPath);
+                    xmlWriter.WriteAttributeString("SourceCreatedUtc", File.GetCreationTimeUtc(inputFile).ToString("o"));
+                    xmlWriter.WriteAttributeString("SourceModifiedUtc", File.GetLastWriteTimeUtc(inputFile).ToString("o"));
+
+                    int ln = 0;
+                    var inClass = false;
+                    var outPath = Path.Combine(args[1], Path.GetFileNameWithoutExtension(inputFile) + ".cs");
+                    StringGroup group = null;
+                    using (var input = new StreamReader(inputFile))
+                    using (var output = new StreamWriter(outPath, false, Encoding.UTF8)) {
+                        while (!input.EndOfStream) {
+                            var line = input.ReadLine();
+                            ln++;
+                            if (string.IsNullOrWhiteSpace(line)) {
+                                output.WriteLine();
+                                continue;
+                            }
+
+                            line = line.Trim();
+                            if (line.StartsWith("//")) {
+                                output.WriteLine(line);
+                                continue;
+                            }
+
+                            try {
+                                if (group == null) {
+                                    if (UsingRegex.IsMatch(line)) {
+                                        if (inClass) {
+                                            Console.Error.WriteLine($"{args[0]}({ln}): Cannot add new using statements after defining a string");
+                                            Environment.Exit(5);
+                                        } else {
+                                            output.WriteLine(line);
+                                        }
                                     } else {
-                                        output.WriteLine(line);
+                                        var fsm = FunctionSignatureRegex.Match(line);
+                                        if (fsm.Success) {
+                                            group = new StringGroup(fsm);
+                                        } else {
+                                            Console.Error.WriteLine($"{args[0]}({ln}): Unrecognized line: {line}");
+                                            Environment.Exit(2);
+                                        }
                                     }
                                 } else {
-                                    var fsm = FunctionSignatureRegex.Match(line);
-                                    if (fsm.Success) {
-                                        group = new StringGroup(fsm);
+                                    var sm = StringRegex.Match(line);
+                                    if (sm.Success) {
+                                        if (!inClass) {
+                                            output.WriteLine("using System;");
+                                            output.WriteLine("using System.Text;");
+                                            output.WriteLine("using Squared.FString;");
+                                            output.WriteLine();
+                                            output.WriteLine("public static partial class FStrings {");
+                                            inClass = true;
+                                        }
+
+                                        var fs = new FString(group, sm);
+                                        xmlWriter.WriteStartElement(fs.Name);
+                                        fs.Write(output);
+                                        xmlWriter.WriteString(fs.FormatString);
+                                        xmlWriter.WriteEndElement();
+                                    } else if (line == "}") {
+                                        group = null;
+                                        // End of group
                                     } else {
                                         Console.Error.WriteLine($"{args[0]}({ln}): Unrecognized line: {line}");
-                                        Environment.Exit(2);
+                                        Environment.Exit(3);
                                     }
                                 }
-                            } else {
-                                var sm = StringRegex.Match(line);
-                                if (sm.Success) {
-                                    if (!inClass) {
-                                        output.WriteLine("using Squared.FString;");
-                                        output.WriteLine();
-                                        output.WriteLine("public static partial class FStrings {");
-                                        inClass = true;
-                                    }
-
-                                    var fs = new FString(group, sm);
-                                    fs.Write(output);
-                                } else if (line == "}") {
-                                    group = null;
-                                    // End of group
-                                } else {
-                                    Console.Error.WriteLine($"{args[0]}({ln}): Unrecognized line: {line}");
-                                    Environment.Exit(3);
-                                }
+                            } catch (Exception exc) {
+                                Console.Error.WriteLine($"{args[0]}({ln}): {exc}");
+                                Environment.Exit(4);
                             }
-                        } catch (Exception exc) {
-                            Console.Error.WriteLine($"{args[0]}({ln}): {exc}");
-                            Environment.Exit(4);
                         }
+
+                        if (inClass)
+                            output.WriteLine("}");
                     }
 
-                    if (inClass)
-                        output.WriteLine("}");
+                    xmlWriter.WriteEndElement();
                 }
-            }
+
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndDocument();
+            }            
         }
     }
 
@@ -107,41 +142,48 @@ namespace FStringCompiler {
 
     public class FString {
         public StringGroup Group;
-        public string Name, RawText;
+        public FStringDefinition Definition;
+        public string Name, FormatString;
 
         public FString (StringGroup group, Match m) {
             Group = group;
             Name = m.Groups["name"].Value;
-            RawText = m.Groups["text"].Value;
+            FormatString = m.Groups["text"].Value;
+            Definition = FStringDefinition.Parse(Name, FormatString);
         }
 
         public void Write (StreamWriter output) {
-            output.Write($"\tpublic static FStringBuilder {Name} (FStringBuilder output");
+            output.WriteLine($"\tpublic struct {Name} : IFString {{");
+            output.WriteLine($"\t\tpublic string Name => \"{Name}\";");
             foreach (var arg in Group.Arguments)
-                output.Write($", {arg.Type} {arg.Name}");
-            output.WriteLine(") {");
-            // FIXME
-            output.Write("\t\toutput.Append($\"");
-            output.Write(RawText);
-            output.WriteLine("\");");
-            output.WriteLine("\t\treturn output;");
-            output.WriteLine("\t}");
+                output.WriteLine($"\t\tpublic {arg.Type} {arg.Name};");
             output.WriteLine();
 
-            output.Write($"\tpublic static string {Name} (");
-            var isFirst = true;
-            foreach (var arg in Group.Arguments) {
-                if (!isFirst)
-                    output.Write(", ");
-                isFirst = false;
-                output.Write($"{arg.Type} {arg.Name}");
+            output.WriteLine("\t\tpublic void EmitValue (ref FStringBuilder output, string id) {");
+            output.WriteLine("\t\t\tswitch(id) {");
+            var keys = Definition.Opcodes.Where(o => o.emit).Select(o => o.textOrId).Distinct();
+            foreach (var key in keys) {
+                output.WriteLine($"\t\t\t\tcase \"{key}\":");
+                output.WriteLine($"\t\t\t\t\toutput.Append({key});");
+                output.WriteLine($"\t\t\t\t\treturn;");
             }
-            output.WriteLine(") {");
-            output.Write($"\t\treturn {Name}(new FStringBuilder()");
-            foreach (var arg in Group.Arguments)
-                output.Write($", {arg.Name}");
-            output.Write(").ToString();");
-            output.WriteLine("\t\t");
+            output.WriteLine("\t\t\t\tdefault:");
+            output.WriteLine("\t\t\t\t\tthrow new ArgumentOutOfRangeException(nameof(id));");
+            output.WriteLine("\t\t\t}");
+            output.WriteLine("\t\t}");
+
+            output.WriteLine("\t\tpublic void AppendTo (ref FStringBuilder output, FStringDefinition definition) => definition.AppendTo(ref this, ref output);");
+            output.WriteLine("\t\tpublic void AppendTo (StringBuilder output) {");
+            output.WriteLine("\t\t\tvar fsb = new FStringBuilder(output);");
+            output.WriteLine("\t\t\tFStringTable.Default.Get(Name).AppendTo(ref this, ref fsb);");
+            output.WriteLine("\t\t}");
+
+            output.WriteLine("\t\tpublic override string ToString () {");
+            output.WriteLine("\t\t\tvar output = new FStringBuilder();");
+            output.WriteLine("\t\t\tAppendTo(ref output, FStringTable.Default.Get(Name));");
+            output.WriteLine("\t\t\treturn output.ToString();");
+            output.WriteLine("\t\t}");
+
             output.WriteLine("\t}");
             output.WriteLine();
         }
