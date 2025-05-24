686d30b1260c4b315af7c668b47b5a24c42289fa
Katelyn Gadd <kg@luminance.org>
2025-05-24 10:57:13 -0700

    Unboxed fstring append helper is probably not worth the trouble and is not NativeAOT compatible

--- FString/FStringBuilder.cs
+++ FString/FStringBuilder.cs
@@ -179,44 +179,8 @@ namespace Squared.FString {
             text.Value.CopyTo(O);
         }
 
-        static class UnboxedFStringAppendHelper {
-            private static readonly Dictionary<Type, IUnboxedFStringAppendHelper> Helpers = 
-                new Dictionary<Type, IUnboxedFStringAppendHelper>(new ReferenceComparer<Type>());
-
-            public static bool TryAppend<T> (ref FStringBuilder output, ref T value) {
-                var t = typeof(T);
-                bool foundCacheEntry;
-                IUnboxedFStringAppendHelper helper;
-
-                lock (Helpers)
-                    foundCacheEntry = Helpers.TryGetValue(t, out helper);
-
-                if (!foundCacheEntry) {
-                    if (typeof(IFString).IsAssignableFrom(t)) {
-                        var helperType = typeof(UnboxedFStringAppendHelper<>).MakeGenericType(t);
-                        helper = (IUnboxedFStringAppendHelper)Activator.CreateInstance(helperType);
-                    }
-
-                    lock (Helpers)
-                        Helpers[t] = helper;
-                }
-
-                if (helper == null)
-                    return false;
-
-                helper.Append(ref output, __makeref(value));
-                return true;
-            }
-        }
-
-        interface IUnboxedFStringAppendHelper {
-            void Append (ref FStringBuilder output, TypedReference value);
-        }
-        
-        class UnboxedFStringAppendHelper<T> : IUnboxedFStringAppendHelper where T : IFString {
-            public void Append (ref FStringBuilder output, TypedReference value) {
-                __refvalue(value, T).AppendTo(ref output);
-            }
+        public void AppendFString<T> (T value)
+            where T : struct, IFString {
         }
         
         public void Append<T> (T value) {
@@ -226,8 +190,8 @@ namespace Squared.FString {
                     Append(cachedName);
                 else
                     Append(value.ToString());
-            } else if (UnboxedFStringAppendHelper.TryAppend(ref this, ref value)) {
-                ;
+            } else if (value is IFString ifs) {
+                ifs.AppendTo(ref this);
             } else if (t.IsValueType) {
                 // We do a valuetype check to avoid the boxing operation necessary to do 'value != null' below
                 Append(value.ToString());
